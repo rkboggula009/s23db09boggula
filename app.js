9b958a4d2208846ac22238a263cb7eb69d02eb2d
rkboggula009 <123134697+rkboggula009@users.noreply.github.com>
2023-04-05 22:23:37 -0500

    updated

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var selectorRouter = require('./routes/selector');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
