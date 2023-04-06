d999412dd56c9524cf9fd4c71bc90a231bb5b790
rkboggula009 <123134697+rkboggula009@users.noreply.github.com>
2023-04-05 22:31:11 -0500

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var glassRouter = require('./routes/images');
+var imageRouter = require('./routes/images');
 var boardRouter = require('./routes/board');
 var selectorRouter = require('./routes/selector');
 
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/images', glassRouter);
+app.use('/images', imageRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 
