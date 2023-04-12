91de033a38f43d9d6b0f0c968ae8bbd1bda575fd
rkboggula009 <123134697+rkboggula009@users.noreply.github.com>
2023-04-12 17:46:00 -0500

    Use a good message

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+const connectionString =
+process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+{useNewUrlParser: true,
+useUnifiedTopology: true});
+
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var imageRouter = require('./routes/images');
