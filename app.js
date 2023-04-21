d567b891e184d8ea9b2e06c82583588bfcb7ee4c
rkboggula009 <123134697+rkboggula009@users.noreply.github.com>
2023-04-21 11:47:04 -0500

    updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
@@ -48,6 +49,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
