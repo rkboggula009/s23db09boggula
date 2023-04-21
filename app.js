2bf809c58cf0e1592c0c0ede51709176ca83954e
rkboggula009 <123134697+rkboggula009@users.noreply.github.com>
2023-04-21 11:45:38 -0500

    updated

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+function(username, password, done) {
+Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+    return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+    return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }));
 
 require('dotenv').config();
 const connectionString =
@@ -32,6 +48,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -40,6 +64,14 @@ app.use('/images', imagesRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 
 // catch 404 and forward to error handler
@@ -64,7 +96,7 @@ await images.deleteMany();
 let instance1 = new images({images_name:"image1", size:"large",cost:1000});
 instance1.save().then(doc=> {
 //if(err) return console.error(err);
-console.log("First object saved")}
+  console.log("First object saved")}
 ).catch(err=>{
   console.error(err)
 });
@@ -73,7 +105,7 @@ let instance2 = new
 images({images_name:"image2", size:"medium",cost:700});
 instance2.save().then(doc=> {
 //if(err) return console.error(err);
-console.log("Second object saved")}
+  console.log("Second object saved")}
 ).catch(err=>{
   console.error(err)
 });
@@ -81,7 +113,7 @@ let instance3 = new
 images({images_name:"image3", size:"small",cost:800});
 instance3.save().then(doc=> {
 //if(err) return console.error(err);
-console.log("Third object saved")}
+  console.log("Third object saved")}
 ).catch(err=>{
   console.error(err)
 });
@@ -90,3 +122,6 @@ let reseed = false;
 if (reseed) { recreateDB();}
 
 module.exports = app;
+
+
+
